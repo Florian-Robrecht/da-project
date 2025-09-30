ca9cf2c1f9342e85350e002ddf1735a43cdf70cf
Florian Robrecht <frobrech@uni-muenster.de>
2025-09-30 11:25:54 +0200

    removed cookie from request

--- db_connector.py
+++ db_connector.py
@@ -15,7 +15,6 @@ url = "https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/sta
 querystring = {"limit": "10000", "category": "1-7"}
 
 headers = {
-    #"cookie": "TS0165d0f4=01d513bcd1dc157dd2fe33fbc17e2d7e409a282b140a7a38f5ed7ed1b7bfd1b64464ec5519d71556bd480788d38d898321ba549fb1",
     "accept": "application/json",
     "DB-Api-Key": os.getenv("CLIENT_SECRET"),
     "DB-Client-ID": os.getenv("CLIENT_ID"),
